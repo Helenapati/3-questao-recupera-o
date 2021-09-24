06965e590174f0991c3dee74db9618b6f2628128
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-09-24 09:19:38 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,9 @@
+- Crie uma classe RepositórioPedidos que armazene os pedidos de um supermercado.
+  Essa classe deve ser capaz de gerar um relatório de vendas do dia.
+- A classe principal deve ser capaz de adicionar mais de um pedido, calcular o
+  valor total de cada pedido e gerar o relatório de vendas ao final do di, incluíndo
+  lucro adquirido.
+*/
+ RepositorioPedidos new = armazenamento (i1,i2,i3,i4);
+ Pedidos new= armazenamento +dia;
+ System.out.println("Vendas do dia:"+pedidos);
